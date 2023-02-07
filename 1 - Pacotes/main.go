9e0456aabe476cbf5df941bd28339ce4b756fd67
Cassio de Freitas <cassio.quintino@gmail.com>
2023-02-07 09:41:57 -0300

    working with external packages

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/secondPackage"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Testing modules")
 	secondPackage.Write()
+
+	err := checkmail.ValidateFormat("test@test.com")
+	fmt.Println(err)
 }
