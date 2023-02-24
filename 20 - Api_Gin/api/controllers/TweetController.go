b4658d41eb8359d566233e92e40beeec316db733
Cassio de Freitas <cassio.quintino@gmail.com>
2023-02-24 20:05:47 -0300

    Add route POST -> Create tweet

--- 20 - Api_Gin/api/controllers/TweetController.go
+++ 20 - Api_Gin/api/controllers/TweetController.go
@@ -19,3 +19,15 @@ func NewTweetController() *tweetController {
 func (t *tweetController) FindAll(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, t.tweets)
 }
+
+func (t *tweetController) Create(ctx *gin.Context) {
+	tweet := entities.NewTweet()
+
+	if err := ctx.BindJSON(&tweet); err != nil {
+		return
+	}
+
+	t.tweets = append(t.tweets, *tweet)
+
+	ctx.JSON(http.StatusOK, tweet)
+}
